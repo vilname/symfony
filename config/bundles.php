13f261d4d02201bc088ac4a0341b9ef426247336
alexander <tchebaeff.alexander@yandex.ru>
2021-06-03 18:41:59 +0300

    включил настройки Sentry, RabbitMQ, Memcached/Redis

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
