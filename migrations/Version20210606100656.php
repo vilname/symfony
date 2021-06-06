5d7a138c52c4997209995d93d5fb35f92b40699d
alexander <tchebaeff.alexander@yandex.ru>
2021-06-06 16:38:00 +0300

    миграция, добавил связку можду сощностями user и group

---
+++ migrations/Version20210606100656.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210606100656 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE group_user (group_id BIGINT NOT NULL, user_id BIGINT NOT NULL, PRIMARY KEY(group_id, user_id))');
+        $this->addSql('CREATE INDEX IDX_A4C98D39FE54D947 ON group_user (group_id)');
+        $this->addSql('CREATE INDEX IDX_A4C98D39A76ED395 ON group_user (user_id)');
+        $this->addSql('ALTER TABLE group_user ADD CONSTRAINT FK_A4C98D39FE54D947 FOREIGN KEY (group_id) REFERENCES "group" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE group_user ADD CONSTRAINT FK_A4C98D39A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE group_user');
+    }
+}
